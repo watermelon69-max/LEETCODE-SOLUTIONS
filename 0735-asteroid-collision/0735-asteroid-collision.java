6a980675892596385c06a29f212d10223b279615
Anurag <anuragmourya901@gmail.com>
2026-07-27 17:08:12 +0530

    Time: 2 ms (98.14%), Space: 46.8 MB (84.41%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,24 +1,25 @@
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        ArrayList<Integer>st=new ArrayList<>();
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]>0) st.add(asteroids[i]);
-            else{
-                while(!st.isEmpty()&&st.get(st.size()-1)>0 && st.get(st.size()-1)<Math.abs(asteroids[i])){
-                    st.remove(st.size()-1);
-                }
-            
-            if(!st.isEmpty() && st.get(st.size()-1)==Math.abs(asteroids[i])){
-                st.remove(st.size()-1);
-            }
-            else if(st.isEmpty()|| st.get(st.size()-1)<0){
+        ArrayList<Integer> st = new ArrayList<>();
+        for (int i = 0; i < asteroids.length; i++) {
+            if (asteroids[i] > 0)
                 st.add(asteroids[i]);
-            }
+            else {
+                while (!st.isEmpty() && st.get(st.size() - 1) > 0 && st.get(st.size() - 1) < Math.abs(asteroids[i])) {
+                    st.remove(st.size() - 1);
+                }
+
+                if (!st.isEmpty() && st.get(st.size() - 1) == Math.abs(asteroids[i])) {
+                    st.remove(st.size() - 1);
+                } else if (st.isEmpty() || st.get(st.size() - 1) < 0) {
+                    st.add(asteroids[i]);
+                }
             }
         }
-        int result[]=new int[st.size()];
-        for(int i=0;i<st.size();i++){
-            result[i]=st.get(i);
+        int result[] = new int[st.size()];
+        for (int i = 0; i < st.size(); i++) {
+            result[i] = st.get(i);
         }
         return result;
     }
