5c8405d13124e19e3c9142bcd98f05e632da368a
Anurag <anuragmourya901@gmail.com>
2026-09-11 09:20:13 +0530

    Time: 38 ms (9.17%), Space: 46.4 MB (15.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+      int dp[]=new int[n+1];
+      Arrays.fill(dp,-1);
+
+      if(n<=1) return n;
+      if(dp[n]!=-1) return dp[n];
+      return fib(n-1)+fib(n-2);
     }
 }
